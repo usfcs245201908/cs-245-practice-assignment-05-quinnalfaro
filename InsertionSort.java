c3fdeed87f40acc0b69cda5ea17dcf65b478fd54
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-09-25 14:13:22 -0700

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,15 @@
+public class InsertionSort implements SortingAlgorithm {
+
+
+	public void sort(int [] a){
+		for(int i = 1; i < a.length; i++){
+			int key = a[i];
+			int j = i - 1;
+			while(j >= 0 && key < a[j]){
+				a[j + 1] = a[j];
+				j --;
+			}
+			a[j + 1] = key;
+		}	
+	}
+}
